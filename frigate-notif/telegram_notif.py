e68b9ced387cf20c05ba705fb9458907440dc420
syidiq <syidiqnet@gmail.com>
2026-04-07 11:38:59 +0700

    fix: update configuration for face recognition and Telegram chat ID

--- frigate-notif/telegram_notif.py
+++ frigate-notif/telegram_notif.py
@@ -13,8 +13,9 @@ FRIGATE_API_URL = f"http://{IP_SERVER}:5002/api/events"
 
 # --- CONFIG TELEGRAM ---
 TELE_TOKEN = "7972577129:AAEzm6U7ZZyIvL-GxuD6lZJrj2zQzt7Rb7s"
-TELE_CHAT_ID = "-5216156976"
-# "5997051893"
+TELE_CHAT_ID = "5997051893"
+# "-5216156976" group telegram
+# "5997051893" personlal telegram
 
 def send_telegram(label, camera, event_id, score, start_time):
     time_wib = datetime.fromtimestamp(start_time, wib).strftime("%H:%M:%S %d/%m/%Y")
